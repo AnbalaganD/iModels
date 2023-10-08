11178bb01344d0593c97e0de99272b61850a0ca8
Anbalagan D <anbu94p@gmail.com>
2023-10-08 12:57:06 +0530

    1. Remove UIKit dependency 2. Use latest swift feature "return allowed in switch expression" 3. Convert UIDevice extension into separate enum. So now we can use without UIKit dependency anywhere.

--- Example/iModels/ViewController.swift
+++ Example/iModels/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print(UIDevice.current.modelName)
+        print(Device.modelName)
     }
 
     override func didReceiveMemoryWarning() {
--- Sources/iModels/Classes/UIDeviceExtension.swift
+++ Sources/iModels/Classes/UIDeviceExtension.swift
@@ -6,17 +6,16 @@
 //
 
 import Foundation
-import UIKit
 
-public extension UIDevice {
-    var modelName: String {
+public enum Device {
+    static public var modelName: String {
 #if targetEnvironment(simulator)
         let identifier = ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"]!
         return identifier
 #else
         var systemInfo = utsname()
         uname(&systemInfo)
-        
+
         let machineMirror = Mirror(reflecting: systemInfo.machine)
         let identifier = machineMirror.children.reduce("") { identifier, element in
             guard let value = element.value as? Int8, value != 0 else {
@@ -24,51 +23,51 @@ public extension UIDevice {
             }
             return identifier + String(UnicodeScalar(UInt8(value)))
         }
-        
-        switch identifier {
-            case "iPhone1,1": return "iPhone 1G"
-            case "iPhone1,2": return "iPhone 3G"
-            case "iPhone2,1": return "iPhone 3GS"
-            case "iPhone3,1", "iPhone3,2", "iPhone3,3": return "iPhone 4"
-            case "iPhone4,1": return "iPhone 4s"
-            case "iPhone5,1", "iPhone5,2": return "iPhone 5"
-            case "iPhone5,3", "iPhone5,4": return "iPhone 5c"
-            case "iPhone6,1", "iPhone6,2": return "iPhone 5s"
-            case "iPhone7,2": return "iPhone 6"
-            case "iPhone7,1": return "iPhone 6 Plus"
-            case "iPhone8,1": return "iPhone 6s"
-            case "iPhone8,2": return "iPhone 6s Plus"
-            case "iPhone8,4": return "iPhone SE"
-            case "iPhone9,1", "iPhone9,3": return "iPhone 7"
-            case "iPhone9,2", "iPhone9,4": return "iPhone 7 Plus"
-            case "iPhone10,1", "iPhone10,4": return "iPhone 8"
-            case "iPhone10,2", "iPhone10,5": return "iPhone 8 Plus"
-            case "iPhone10,3", "iPhone10,6": return "iPhone X"
-            case "iPhone11,2": return "iPhone XS"
-            case "iPhone11,4", "iPhone11,6": return "iPhone XS Max"
-            case "iPhone11,8": return "iPhone XR"
-            case "iPhone12,1": return "iPhone 11"
-            case "iPhone12,3": return "iPhone 11 Pro"
-            case "iPhone12,5": return "iPhone 11 Pro Max"
-            case "iPhone12,8": return "iPhone SE (2nd generation)"
-            case "iPhone13,1": return "iPhone 12 mini"
-            case "iPhone13,2": return "iPhone 12"
-            case "iPhone13,3": return "iPhone 12 Pro"
-            case "iPhone13,4": return "iPhone 12 Pro Max"
-            case "iPhone14,2": return "iPhone 13 Pro"
-            case "iPhone14,3": return "iPhone 13"
-            case "iPhone14,4": return "iPhone 13 mini"
-            case "iPhone14,5": return "iPhone 13 Pro Max"
-            case "iPad1,1": return "iPad"
-            case "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4": return "iPad 2"
-            case "iPad3,1", "iPad3,2", "iPad3,3": return "iPad (3rd generation)"
-            case "iPad3,4", "iPad3,5", "iPad3,6": return "iPad (4th generation)"
-            case "iPad6,11", "iPad6,12": return "iPad (5th generation)"
-            case "iPad7,5", "iPad7,6": return "iPad (6th generation)"
-            case "iPad7,11", "iPad7,12": return "iPad (7th generation)"
-            case "iPad11,6", "iPad11,7": return "iPad (8th generation)"
-            case "iPad12,1", "iPad12,2": return "iPad (9th generation)"
-            default: return identifier
+
+        return switch identifier {
+            case "iPhone1,1": "iPhone 1G"
+            case "iPhone1,2": "iPhone 3G"
+            case "iPhone2,1": "iPhone 3GS"
+            case "iPhone3,1", "iPhone3,2", "iPhone3,3": "iPhone 4"
+            case "iPhone4,1": "iPhone 4s"
+            case "iPhone5,1", "iPhone5,2": "iPhone 5"
+            case "iPhone5,3", "iPhone5,4": "iPhone 5c"
+            case "iPhone6,1", "iPhone6,2": "iPhone 5s"
+            case "iPhone7,2": "iPhone 6"
+            case "iPhone7,1": "iPhone 6 Plus"
+            case "iPhone8,1": "iPhone 6s"
+            case "iPhone8,2": "iPhone 6s Plus"
+            case "iPhone8,4": "iPhone SE"
+            case "iPhone9,1", "iPhone9,3": "iPhone 7"
+            case "iPhone9,2", "iPhone9,4": "iPhone 7 Plus"
+            case "iPhone10,1", "iPhone10,4": "iPhone 8"
+            case "iPhone10,2", "iPhone10,5": "iPhone 8 Plus"
+            case "iPhone10,3", "iPhone10,6": "iPhone X"
+            case "iPhone11,2": "iPhone XS"
+            case "iPhone11,4", "iPhone11,6": "iPhone XS Max"
+            case "iPhone11,8": "iPhone XR"
+            case "iPhone12,1": "iPhone 11"
+            case "iPhone12,3": "iPhone 11 Pro"
+            case "iPhone12,5": "iPhone 11 Pro Max"
+            case "iPhone12,8": "iPhone SE (2nd generation)"
+            case "iPhone13,1": "iPhone 12 mini"
+            case "iPhone13,2": "iPhone 12"
+            case "iPhone13,3": "iPhone 12 Pro"
+            case "iPhone13,4": "iPhone 12 Pro Max"
+            case "iPhone14,2": "iPhone 13 Pro"
+            case "iPhone14,3": "iPhone 13"
+            case "iPhone14,4": "iPhone 13 mini"
+            case "iPhone14,5": "iPhone 13 Pro Max"
+            case "iPad1,1": "iPad"
+            case "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4": "iPad 2"
+            case "iPad3,1", "iPad3,2", "iPad3,3": "iPad (3rd generation)"
+            case "iPad3,4", "iPad3,5", "iPad3,6": "iPad (4th generation)"
+            case "iPad6,11", "iPad6,12": "iPad (5th generation)"
+            case "iPad7,5", "iPad7,6": "iPad (6th generation)"
+            case "iPad7,11", "iPad7,12": "iPad (7th generation)"
+            case "iPad11,6", "iPad11,7": "iPad (8th generation)"
+            case "iPad12,1", "iPad12,2": "iPad (9th generation)"
+            default: identifier
         }
 #endif
     }
